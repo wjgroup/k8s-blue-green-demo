a8afeb53fed1b58ed73454208f4ae803fcec308c
WJ <wj.group@hotmail.com>
2019-09-07 10:58:43 +1000

    add demo web app

---
+++ netcoreweb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace netcoreweb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
